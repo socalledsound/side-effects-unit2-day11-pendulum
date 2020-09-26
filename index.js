1bef16f0a54490940bff5cc501a2ebb50e6f085a
socalled kubick <socalledsound@gmail.com>
2020-09-26 16:47:25 -0700

    multiple pendulums

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 
 const gravity = 0.8;
-const myPendulum = new Pendulum(400, 400, 50, gravity);
+// const myPendulum = new Pendulum(400, 400, 50, gravity);
+
+const pendulums = Array.from({ length: 4 },(e, i)=> {
+    return new Pendulum(400, (i+1)*100, (i+1)*30, gravity)
+});
+
 
 function setup(){
     createCanvas(800, 800);
@@ -8,6 +13,10 @@ function setup(){
 
 function draw(){
     background(220, 220, 250);
-    myPendulum.update();
-    myPendulum.display();
+
+    pendulums.forEach(pendulum => {
+        pendulum.update();
+        pendulum.display();
+    })
+
 }
