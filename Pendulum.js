a2c385b9b0dee34f4875dbd8a28800095f4cacfe
socalled kubick <socalledsound@gmail.com>
2020-09-27 12:23:02 -0700

    comments

--- Pendulum.js
+++ Pendulum.js
@@ -1,12 +1,16 @@
 class Pendulum {
+    //the constructor method takes in the values that we pass in when we call new Pendulum()
     constructor(originX, frequency, ballRadius, gravity, index){
+        //the starting point for the line, which will be 
         this.originX = originX;
         this.originY = 0;
         this.ballX = originX + frequency;
         this.ballY = this.originY + frequency;
         this.ballRadius = ballRadius;
         this.gravity = gravity;
+        //frequency is controlled by the length of the line
         this.frequency = frequency;
+        //the starting angle for our pendulum
         this.angle = Math.PI/2;
         this.angleVelocity = 0.0;
         this.angleAcceleration = 0.0;
@@ -14,12 +18,15 @@ class Pendulum {
         this.strokeColor = [140,40,140];
         this.strokeWeight = 6;
         this.fillColor = [200,60,120];
+        //we'll set these to null to start with but supply the values in our setup function
+        //and then update them in our update loop
         this.sound = null;
-        this.ampMultiplier = index/10;
+        this.ampMultiplier = index/100;
         this.lfo = null;
         // console.log(this.angle);
     }
 
+    //the pendulum math is really all about the first line of this function which updates the angle acceleration
     updateAngle(){
         this.angleAcceleration = (-1 * gravity / this.frequency) * sin(this.angle);  // Calculate acceleration (see: http://www.myphysicslab.com/pendulum1.html)
         this.angleVelocity += this.angleAcceleration;                            // Increment velocity
@@ -28,21 +35,23 @@ class Pendulum {
     }
 
     updatePosition(){
-        // this.position.set(this.frequency * sin(this.currentAngle), this.frequency * cos(this.angle), 0); 
-        // console.log(this.angle);
+        //these are the formulas we were using before to make a circle
+        //but our angleAcceleration calculation keeps 
         this.ballX = Math.sin(this.angle) * this.frequency + this.originX;
         this.ballY = Math.cos(this.angle) * this.frequency; 
     }
+
 
 
     updateSound(){
         // console.log(this.sound);
         this.amp = Math.abs(Math.sin(Math.pow(4, this.angle)));
          console.log(this.amp);
-        this.sound.amp(this.amp * 0.1 * this.ampMultiplier);
+        this.sound.amp(this.amp * this.ampMultiplier);
         this.sound.freq(this.lfo);
     }
 
+    //I call this meta update function in the main draw loop and it calls the other update functions
     update(){
         this.updateAngle();
         this.updatePosition();
--- index.js
+++ index.js
@@ -2,18 +2,20 @@ const numPendulums = 4;
 const gravity = 0.8;
 // const myPendulum = new Pendulum(400, 400, 50, gravity);
 
+//make an array of four pendulums
 const pendulums = Array.from({ length: numPendulums },(e, i)=> {
     return new Pendulum(400, (i+1)*100, (i+1)*30, gravity, i)
 });
 
+//i made these values as I was playing with values to get more interesting sounds
 const reversePendulums = pendulums.reverse();
-
 const lfos = [500,1000,5000,10000];
 
 
 function setup(){
     createCanvas(800, 800);
 
+    //make a new sound for each pendulum
 pendulums.forEach((pendulum, i) => {
 
 
@@ -44,6 +46,7 @@ pendulums.forEach((pendulum, i) => {
 function draw(){
     background(220, 220, 250);
 
+    //update and display the array of pendulums
     pendulums.forEach(pendulum => {
         pendulum.update();
         pendulum.display();
